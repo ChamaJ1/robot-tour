48ddac6b8f182302834f8cf55cfc2662b54e6a56
ChamaJ1 <149020386+ChamaJ1@users.noreply.github.com>
2025-02-07 23:19:11 -0500

    main.py

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-def foward50():
-    robot.motor_tank(50, 44, 1700)
-    robot.motor_stop()
-def leftTurn():
-    robot.motor_steer(90, 100, 500)
-    robot.motor_stop()
+# input code for SO here
 
 def on_button_pressed_a():
-    
-    foward25()
+    pass
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def foward50():
+    robot.motor_tank(44, 50, 1700)
+    robot.motor_stop()
+def leftTurn():
+    robot.motor_stop()
+    basic.pause(1000)
+    robot.motor_tank(19, 0, 275)
+    robot.motor_stop()
 def rightTurn():
-    robot.inksmith_k8.motor_steer(-90, 60, 500)
-    robot.motor_steer(-90, 60, 500)
+    robot.motor_stop()
+    basic.pause(1000)
+    robot.motor_tank(0, 30, 1100)
     robot.motor_stop()
 def foward25():
-    # note for tmr, try to find a way to make the motors have the same power output, not speed
-    robot.motor_tank(50, 50, 850)
+    robot.motor_tank(44, 50, 950)
     robot.motor_stop()
 def uTurn():
-    robot.motor_steer(90, 100, 850)
+    robot.motor_tank(0, 50, 1000)
     robot.motor_stop()
 robot.inksmith_k8.start()
+basic.clear_screen()
