f511d7bd0ae3b87aaeb18b5acb06927b99166f64
ChamaJ1 <149020386+ChamaJ1@users.noreply.github.com>
2025-02-07 23:09:46 -0500

    main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+//input code for SO here
+
+
+})
+
 function foward50 () {
     robot.motorTank(44, 50, 1700)
     robot.motorStop()
@@ -8,26 +14,15 @@ function leftTurn () {
     robot.motorTank(19, 0, 275)
     robot.motorStop()
 }
-input.onButtonPressed(Button.A, function () {
-    foward25()
-    foward25()
-    foward25()
-    rightTurn()
-    rightTurn()
-    foward25()
-    foward25()
-    foward25()
-})
+
 function rightTurn () {
     robot.motorStop()
     basic.pause(1000)
     robot.motorTank(0, 30, 1100)
     robot.motorStop()
 }
+
 function foward25 () {
-    // note for tmr, try to find a way to make the motors have the same power output, not speed
-    robot.motorStop()
-    basic.pause(1000)
     robot.motorTank(44, 50, 950)
     robot.motorStop()
 }
@@ -36,3 +31,4 @@ function uTurn () {
     robot.motorStop()
 }
 robot.inksmithK8.start()
+basic.clearScreen()
